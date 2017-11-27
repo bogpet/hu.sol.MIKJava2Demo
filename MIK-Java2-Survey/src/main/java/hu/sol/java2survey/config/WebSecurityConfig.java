96292fdcefcdba3a925b6a6411115d14b1ea6f7d
Hamerli András <andras.hamerli@suitsolutions.eu>
2017-11-27 14:19:04 +0100

    Refactor

--- MIK-Java2-Survey/src/main/java/hu/sol/java2survey/config/WebSecurityConfig.java
+++ MIK-Java2-Survey/src/main/java/hu/sol/java2survey/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private DataSource dataSource;
 
